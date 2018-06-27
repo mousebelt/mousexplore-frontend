14d93798f2d0e70ba664f5a1375e51f7cbaabe62
Charn Lee <crazydev@aol.com>
2018-06-27 11:59:33 -0700

    Split Address container

---
+++ nrlxplore/src/containers/AddressContainer/NEOAddress.js
@@ -0,0 +1,106 @@
+import React, { PureComponent } from 'react';
+import { connectSettings } from 'core';
+
+import Address from 'components/Address/Address';
+
+class NEOAddress extends PureComponent {
+  state = {
+    address: undefined,
+    balance: undefined,
+    totalTxns: undefined,
+    txnHistory: []
+  };
+
+  componentDidMount() {
+    const { apiObject, currency, address } = this.props;
+
+    if (address) {                            
+      this.getAddressInfo(apiObject, currency, address);
+    }
+  }
+
+  getAddressInfo (apiObject, currency, address) {
+    this.setState({address});
+
+    apiObject.get(`/balance/${address}`)
+      .then(res => {
+        if (res.data.status !== 200) return;
+
+        this.setState({
+          balance: res.data.data.balance
+        });
+      })
+    this.getAddressTxns(apiObject, currency, address)
+  }
+
+  getAddressTxns (apiObject, currency, address) {
+    
+    const { txnHistory } = this.state;
+
+    apiObject.get(`/address/txs/${address}`, {
+      offset: txnHistory.length
+    })
+      .then(res => {
+        if (res.data.status !== 200) return;
+
+        let { total: totalTxns, result: newTxns } = res.data.data;
+        
+        newTxns = newTxns.map(txn => {
+          let value = 0;
+
+          txn.vin.forEach( vin => {
+            if (vin.address && (vin.address.address === address))
+              value -= (+vin.address.value);
+          });
+
+          txn.vout.forEach( vout => {
+            if (vout.address && (vout.address === address))
+              value += (+vout.value);
+          });
+          
+          return {
+            hash: txn.txid,
+            blockHash: txn.blockhash,
+            timestamp: txn.blocktime,
+            value: value
+          }
+        });
+        
+        this.setState({
+          totalTxns, 
+          txnHistory: txnHistory.concat(newTxns)
+        });
+      })
+  }
+
+  handleViewMore = () => {
+    const { apiObject, currency, address } = this.props;
+
+    if (address) {
+      this.getAddressTxns(apiObject, currency, address);    
+    }
+  }
+
+  render () {
+    const { currency } = this.props;
+    const { address, balance, txnHistory, totalTxns } = this.state;
+    return (
+      <Address
+        currency={currency}
+        address={address}
+        balance={balance}
+        txnHistory={txnHistory}
+        totalTxns={totalTxns}
+        onViewMore={this.handleViewMore}
+      />
+    );
+  }
+}
+
+const mapStateToProps = ({settings}) => ({
+  currency: settings.currency,
+  apiObject: settings.apiObject
+});
+
+
+export default connectSettings(mapStateToProps, {})(NEOAddress);
