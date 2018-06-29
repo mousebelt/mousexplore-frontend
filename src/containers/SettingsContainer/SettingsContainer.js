0cfa44232293e8818191e063c11988fe2bea6ce7
crazydev71 <crazy.dev@aol.com>
2018-06-28 19:35:03 -0700

    Integrate with search api

--- src/containers/SettingsContainer/SettingsContainer.js
+++ src/containers/SettingsContainer/SettingsContainer.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react';
+import { compose } from 'recompose';
+import { withRouter } from 'react-router-dom';
 import { connectSettings } from 'core';
+import { notification } from 'antd';
+import qs from 'query-string';
 
 import OwnerLogo from './OwnerLogo/OwnerLogo';
 import CurrencySelect from './CurrencySelect/CurrencySelect';
@@ -8,7 +11,39 @@ import NetTypeToggle from './NetTypeToggle/NetTypeToggle';
 import TokenSelect from './TokenSelect/TokenSelect';
 import SearchBar from 'components/SearchBar/SearchBar';
 
-class SettingsContainer extends Component {
+class SettingsContainer extends PureComponent {
+  handleSearch = (searchString) => {
+
+    const { history, settings } = this.props;
+    const { apiObject } = settings;
+
+    apiObject.get(`/search/${searchString}`)
+      .then(res => {
+        if (res.data.status !== 200)
+          throw Error('Invalid search string');
+
+        const type = res.data.data.type;
+
+        const queryString = qs.stringify({
+          net: (settings.netType.toLowerCase() === 'test') ? settings.netType.toLowerCase() : undefined,
+          ticker: settings.ticker ? settings.ticker.toLowerCase() : undefined,
+        });
+
+        const newLocation = history.createHref({
+          pathname: `/${settings.currency.toLowerCase()}/${type}/${searchString}`,
+          search: queryString,
+        });
+
+        history.push(newLocation);
+      })
+      .catch(err => {
+        notification.warning({
+          message: err.message,
+          description : `We can't find matching keyword`
+        });
+      })
+  }
+
   render() {
     const { settings, setCurrency, setNetType, setTicker } = this.props;
     return (
@@ -30,25 +65,29 @@ class SettingsContainer extends Component {
           />
         </div>
         <div className="settings__search">
-          <SearchBar />
+          <SearchBar onSearch={this.handleSearch}/>
         </div>
       </div>
     );
   }
 }
 
-SettingsContainer.propTypes = {
+// SettingsContainer.propTypes = {
 
-  // Current state of settings in redux
-  settings: PropTypes.shape({
-    currency: PropTypes.string,
-    netType: PropTypes.string,
-    ticker: PropTypes.string
-  }),
+//   // Current state of settings in redux
+//   settings: PropTypes.shape({
+//     currency: PropTypes.string,
+//     netType: PropTypes.string,
+//     ticker: PropTypes.string,
+//     apiObject: PropTypes.object
+//   }),
 
-  // Action dispatcher to set the state of settings
-  setSettings: PropTypes.func
+//   // Action dispatcher to set the state of settings
+//   setSettings: PropTypes.func
   
-}
+// }
 
-export default connectSettings()(SettingsContainer);
+export default compose(
+  connectSettings(),
+  withRouter
+)(SettingsContainer);
